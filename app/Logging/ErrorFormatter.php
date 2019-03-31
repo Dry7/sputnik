e73bfe87151b1db91210d43d20bdf359d57084d2
Dry7 <andrey.dry7.shilov@yandex.ru>
2019-03-31 14:54:07 +0300

    ErrorFormatter

---
+++ app/Logging/ErrorFormatter.php
@@ -0,0 +1,41 @@
+<?php
+
+declare(strict_types=1);
+
+namespace Sputnik\Logging;
+
+use Monolog\Formatter\FormatterInterface;
+use Sputnik\Exceptions\BaseException;
+
+class ErrorFormatter implements FormatterInterface
+{
+    /**
+     * {@inheritdoc}
+     */
+    public function format(array $record)
+    {
+        $exception = $record['context']['exception'];
+        $context = $exception instanceof BaseException
+            ? $exception->getContext()
+            : [];
+
+        return "\n" . json_encode([
+            'type' => 'error',
+            'timestamp' => $record['datetime']->getTimestamp(),
+            'message' => $record['message'] . ' ' . print_r($context, true),
+        ]);
+    }
+
+    /**
+     * {@inheritdoc}
+     */
+    public function formatBatch(array $records)
+    {
+        foreach ($records as $key => $record) {
+            $records[$key] = $this->format($record);
+        }
+
+        return $records;
+    }
+
+}
