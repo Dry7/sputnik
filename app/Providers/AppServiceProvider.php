cb6fb23f12aa5ff6ba4aaa03cafa7841c2a93c07
Dry7 <andrey.dry7.shilov@yandex.ru>
2019-03-31 14:21:05 +0300

    Refactoring service registration

--- app/Providers/AppServiceProvider.php
+++ app/Providers/AppServiceProvider.php
@@ -2,9 +2,12 @@
 
 namespace Sputnik\Providers;
 
+use GuzzleHttp\Client;
 use Illuminate\Support\ServiceProvider;
+use Sputnik\Services\ExchangeService;
 use Sputnik\Services\FlightProgramService;
 use Sputnik\Services\LoggerService;
+use Sputnik\Services\TelemetryService;
 
 class AppServiceProvider extends ServiceProvider
 {
@@ -15,7 +18,20 @@ class AppServiceProvider extends ServiceProvider
      */
     public function register()
     {
-        $this->app->singleton(FlightProgramService::class);
+        $this->app->singleton(ExchangeService::class, function ($app) {
+            return new ExchangeService(
+                $app[Client::class],
+                config('sputnik.exchange_uri'),
+                config('sputnik.exchange_timeout')
+            );
+        });
+        $this->app->singleton(FlightProgramService::class, function ($app) {
+            return new FlightProgramService(
+                $app[TelemetryService::class],
+                $app[ExchangeService::class],
+                config('sputnik.telemetry_freq')
+            );
+        });
         $this->app->singleton(LoggerService::class);
     }
 }
