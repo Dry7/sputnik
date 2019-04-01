2bf9b86ad091a650f5bbc8370014a0d16cdfdd5d
Dry7 <andrey.dry7.shilov@yandex.ru>
2019-04-02 01:48:50 +0300

    Logger formatters tests

---
+++ tests/Unit/Logging/ErrorFormatterTest.php
@@ -0,0 +1,91 @@
+<?php
+
+namespace Tests\Unit;
+
+use Sputnik\Exceptions\InvalidOperation;
+use Sputnik\Logging\ErrorFormatter;
+use Tests\TestCase;
+use DateTime;
+
+class ErrorFormatterTest extends TestCase
+{
+    /** @var ErrorFormatter */
+    private $formatter;
+
+    public function setUp(): void
+    {
+        $this->formatter = new ErrorFormatter();
+        parent::setUp();
+    }
+
+    public static function formatDataProvider()
+    {
+        return [
+            [
+                'record' => [
+                    'message' => 'No matching handler found',
+                    'datetime' => new DateTime('2013-03-01T16:15:09+03:00'),
+                    'context' => [],
+                ],
+                'expected' => "\n" . '{"type":"error","timestamp":1362143709,"message":"No matching handler found Array\n(\n)\n"}',
+            ],
+            [
+                'record' => [
+                    'message' => 'No matching handler found',
+                    'datetime' => new DateTime('2013-03-06T16:15:09+03:00'),
+                    'context' => [
+                        'post' => 1,
+                        'html' => '<html></html>',
+                        'array' => [
+                            'string' => 'str',
+                        ],
+                    ],
+                ],
+                'expected' => "\n" . '{"type":"error","timestamp":1362575709,"message":"No matching handler found Array\n(\n    [post] => 1\n    [html] => <html><\/html>\n    [array] => Array\n        (\n            [string] => str\n        )\n\n)\n"}',
+            ],
+            [
+                'record' => [
+                    'message' => 'Invalid operation: critical',
+                    'datetime' => new DateTime('2013-03-01T16:15:09+03:00'),
+                    'context' => [
+                        'exception' => InvalidOperation::critical(['new'])
+                    ],
+                ],
+                'expected' => "\n" . '{"type":"error","timestamp":1362143709,"message":"Invalid operation: critical Array\n(\n    [0] => new\n)\n"}',
+            ],
+        ];
+    }
+
+    /**
+     * @dataProvider formatDataProvider
+     *
+     * @param array $record
+     * @param string $expected
+     */
+    public function testFormat(array $record, string $expected)
+    {
+        $this->assertSame($expected, $this->formatter->format($record));
+    }
+
+    public function testFormatBatch()
+    {
+        $records = [
+            [
+                'message' => 'Exception 1',
+                'datetime' => new DateTime('2013-03-01T16:15:09+03:00'),
+                'context' => [],
+            ],
+            [
+                'message' => 'Exception 2',
+                'datetime' => new DateTime('2013-03-01T16:15:09+03:00'),
+                'context' => [],
+            ],
+        ];
+        $expected = [
+            "\n" . '{"type":"error","timestamp":1362143709,"message":"Exception 1 Array\n(\n)\n"}',
+            "\n" . '{"type":"error","timestamp":1362143709,"message":"Exception 2 Array\n(\n)\n"}'
+        ];
+
+        self::assertSame($expected, $this->formatter->formatBatch($records));
+    }
+}
---
+++ tests/Unit/Logging/LogFormatterTest.php
@@ -0,0 +1,110 @@
+<?php
+
+namespace Tests\Unit;
+
+use Monolog\Logger;
+use Sputnik\Exceptions\InvalidOperation;
+use Sputnik\Logging\ErrorFormatter;
+use Sputnik\Logging\LogFormatter;
+use Tests\TestCase;
+use DateTime;
+
+class LogFormatterTest extends TestCase
+{
+    /** @var ErrorFormatter */
+    private $formatter;
+
+    public function setUp(): void
+    {
+        $this->formatter = new LogFormatter();
+        parent::setUp();
+    }
+
+    public static function formatDataProvider()
+    {
+        return [
+            [
+                'record' => [
+                    'message' => 'No matching handler found',
+                    'datetime' => new DateTime('2013-03-01T16:15:09+03:00'),
+                    'context' => [],
+                    'level' => Logger::INFO,
+                ],
+                'expected' => "\n" . '{"time":"2013-03-01T13:15:09Z","type":"info","message":"No matching handler found"}',
+            ],
+            [
+                'record' => [
+                    'message' => 'No matching handler found',
+                    'datetime' => new DateTime('2013-03-06T16:15:09+03:00'),
+                    'context' => [
+                        'post' => 1,
+                        'html' => '<html></html>',
+                        'array' => [
+                            'string' => 'str',
+                        ],
+                    ],
+                    'level' => Logger::INFO,
+                ],
+                'expected' => "\n" . '{"time":"2013-03-06T13:15:09Z","type":"info","message":"No matching handler found {\"post\":1,\"html\":\"<html><\\\\\\/html>\",\"array\":{\"string\":\"str\"}}"}',
+            ],
+            [
+                'record' => [
+                    'message' => 'Invalid operation: critical',
+                    'datetime' => new DateTime('2013-03-01T16:15:09+03:00'),
+                    'context' => [
+                        'exception' => InvalidOperation::critical(['new'])
+                    ],
+                    'level' => Logger::INFO,
+                ],
+                'expected' => "\n" . '{"time":"2013-03-01T13:15:09Z","type":"info","message":"Invalid operation: critical [\"new\"]"}',
+            ],
+        ];
+    }
+
+    /**
+     * @dataProvider formatDataProvider
+     *
+     * @param array $record
+     * @param string $expected
+     */
+    public function testFormat(array $record, string $expected)
+    {
+        $this->assertSame($expected, $this->formatter->format($record));
+    }
+
+    public function testFormatWarning()
+    {
+        $record = [
+            'message' => 'No matching handler found',
+            'datetime' => new DateTime('2013-03-01T16:15:09+03:00'),
+            'context' => [],
+            'level' => Logger::WARNING,
+        ];
+
+        $this->assertSame('', $this->formatter->format($record));
+    }
+
+    public function testFormatBatch()
+    {
+        $records = [
+            [
+                'message' => 'Exception 1',
+                'datetime' => new DateTime('2013-03-01T16:15:09+03:00'),
+                'context' => [],
+                'level' => Logger::INFO,
+            ],
+            [
+                'message' => 'Exception 2',
+                'datetime' => new DateTime('2013-03-01T16:15:09+03:00'),
+                'context' => [],
+                'level' => Logger::INFO,
+            ],
+        ];
+        $expected = [
+            "\n" . '{"time":"2013-03-01T13:15:09Z","type":"info","message":"Exception 1"}',
+            "\n" . '{"time":"2013-03-01T13:15:09Z","type":"info","message":"Exception 2"}',
+        ];
+
+        self::assertSame($expected, $this->formatter->formatBatch($records));
+    }
+}
