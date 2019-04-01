0eacf9b58d16682eb6cb1d58e31f5fc06e77a928
Dry7 <andrey.dry7.shilov@yandex.ru>
2019-04-01 22:33:53 +0300

    Log all requests

--- app/Providers/AppServiceProvider.php
+++ app/Providers/AppServiceProvider.php
@@ -3,6 +3,8 @@
 namespace Sputnik\Providers;
 
 use GuzzleHttp\Client;
+use GuzzleHttp\TransferStats;
+use Illuminate\Support\Facades\Log;
 use Illuminate\Support\ServiceProvider;
 use Sputnik\Services\ExchangeService;
 use Sputnik\Services\FlightProgramService;
@@ -18,6 +20,13 @@ class AppServiceProvider extends ServiceProvider
      */
     public function register()
     {
+        $this->app->singleton(Client::class, function () {
+            return new Client([
+                'on_stats' => function (TransferStats $stats) {
+                    Log::info('Request ' . $stats->getEffectiveUri(), $stats->getHandlerStats());
+                }
+            ]);
+        });
         $this->app->singleton(ExchangeService::class, function ($app) {
             return new ExchangeService(
                 $app[Client::class],
